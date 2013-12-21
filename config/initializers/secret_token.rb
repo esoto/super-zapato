74858f7b0b14544b68be0db901a74b53db6a8282
Esteban Soto <esoto@pernix-solutions.com>
2013-12-20 21:33:07 -0600

    starting the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperZapatos::Application.config.secret_token = 'b82c97282efe5c0574c8c32a3a77a1153b2c2839c6b7b24fb582e29bf1c8d2d9502f50ec04ead06b4f8bde7549becc9e29a720322e6966bdb7b51cc32efd58e0'
